87301fa049d58aa486e5ba1af13288ce810928e4
Missing-Texture <louis.hoeser@gmail.com>
2021-09-27 01:09:21 +0200

    defined icons for duplicate weather codes

--- src/WeatherIcon.tsx
+++ src/WeatherIcon.tsx
@@ -8,13 +8,13 @@ const iconSize = 60
 export default function WeatherIcon({ weatherCode }: { weatherCode: number }) {
 
     switch(weatherCode) {
-        case 113: return <Sunny height={iconSize} width={iconSize} />           // sunny
-        case 116: return <PartlyCloudy height={iconSize} width={iconSize} />    // partly cloudy
-        case 119: return <Cloudy height={iconSize} width={iconSize} />          // cloudy
-        case 122: return <VeryCloudy height={iconSize} width={iconSize} />      // very cloudy
-        case 176: return <LightShowers height={iconSize} width={iconSize} />    // light showers
-        case 297: return <LightRain height={iconSize} width={iconSize} />       // light rain
-        case 302: return <HeavyRain height={iconSize} width={iconSize} />       // heavy showers
-        default: return <PartlyCloudy height={iconSize} width={iconSize} />
+        case 113: return    <Sunny height={iconSize} width={iconSize} />                                                                           // sunny
+        case 116: return    <PartlyCloudy height={iconSize} width={iconSize} />                                                                    // partly cloudy
+        case 119: return    <Cloudy height={iconSize} width={iconSize} />                                                                          // cloudy
+        case 122: return    <VeryCloudy height={iconSize} width={iconSize} />                                                                      // very cloudy
+        case 176: case 263: case 353: return    <LightShowers height={iconSize} width={iconSize} />                                                // light showers
+        case 266: case 293: case 296: return    <LightRain height={iconSize} width={iconSize} />                                                   // light rain
+        case 299: case 302: case 305: case 308: case 356: case 359: return      <HeavyRain height={iconSize} width={iconSize} />                   // heavy rain
+        default: return     <PartlyCloudy height={iconSize} width={iconSize} />
     }
 }
