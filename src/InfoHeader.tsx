864932ea2b5db3a5a15b89d02f5deb3c5c64b41c
Missing-Texture <louis.hoeser@gmail.com>
2021-09-27 18:50:09 +0200

    added city selection modal

--- src/InfoHeader.tsx
+++ src/InfoHeader.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, { useRef, useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Modal, Pressable } from 'react-native';
 
 import { fetchWeatherData } from './FetchData'
 import { IData } from './Interfaces'
@@ -9,19 +9,45 @@ import WeatherIcon from './WeatherIcon';
 
 export default function WeatherData({ data, url, city, setCity, setData, setIsLoading }: { data: IData, url: string, city: string, setCity: any, setData: any, setIsLoading: any }) {
 
+    const cityTextInput = useRef<TextInput>(null)
+
+    const [modalVisible, setModalVisible] = useState(false);
+
     return(
-        <View style={ styles.container }> 
-            <View style={ styles.weatherSubContainer }>
-                <WeatherIcon weatherCode={data?.currentWeatherCode}/>
-                <Text style={[ styles.temperatureText, globalStyles.whiteText ]}>{data?.currentTemp}</Text>
+        <>
+            <View style={ styles.container }> 
+                <View style={ styles.weatherSubContainer }>
+                    <WeatherIcon weatherCode={data?.currentWeatherCode}/>
+                    <Text style={[ styles.temperatureText, globalStyles.whiteText ]}>{data?.currentTemp}</Text>
+                </View>
+                <Pressable onPress={() => setModalVisible(true)}>
+                    <Text style={[ styles.cityText, globalStyles.whiteText ]}>{data?.city}</Text>
+                </Pressable>
             </View>
-            <TextInput 
-                style={[ styles.cityText, globalStyles.whiteText ]}
-                onChangeText={setCity}
-                onEndEditing={() => { fetchWeatherData(url, setData, setIsLoading) }}
-                value={city}
-            />
-        </View>
+
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+                onShow={() => {cityTextInput?.current?.focus()}}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={[ styles.cityText, globalStyles.whiteText ]}>Type in your City: </Text>
+                        <TextInput 
+                            style={[ styles.cityText, globalStyles.whiteText ]}
+                            onChangeText={setCity}
+                            onEndEditing={() => { fetchWeatherData(url, setData, setIsLoading) }}
+                            // value={city}
+                            ref={cityTextInput}
+                        />
+                    </View>
+                </View>     
+            </Modal>
+        </>
     )
 }
 
@@ -45,5 +71,27 @@ const styles = StyleSheet.create({
     cityText: {
         fontSize: 30, 
         fontWeight: 'bold',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalView: {
+        width: '80%',
+        height: '30%',
+        margin: 20,
+        backgroundColor: "black",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
     },
 });
