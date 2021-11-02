711a48bf89f483eb557b7b96725eef1347fd15e0
Missing-Texture <louis.hoeser@gmail.com>
2021-11-02 23:17:07 +0100

    increased possible max and min temps for color gradient wrapped temp gradient code in useEffect

--- src/WeatherData.tsx
+++ src/WeatherData.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native';
 import { AreaChart, XAxis, YAxis } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
@@ -13,51 +13,54 @@ export default function WeatherData(
     { data, maxTemp, minTemp }: 
     { data: IDayWeatherInfo, maxTemp: number, minTemp: number }
 ) {
-    //TODO: increase max and min temps 
-    // max temp is 35, min temp is -20 -> +20 to clap values between 0 and 55
-    let maxGradVal = (1 - ( ( maxTemp + 20 ) / 55 )).toFixed(1)
-    let minGradVal = (1 - ( ( minTemp + 20 ) / 55 )).toFixed(1)
+    const [gradientColors, setGradientColors]: any = useState([])
 
-    console.log("max: " + maxGradVal + ", min: " + minGradVal)
 
-    const colorMappings: any = {
-        0.0: "#E92020",
-        0.2: "#E7872D",
-        0.4: "#E1C627",
-        0.5: "#3ADE4E",
-        0.6: "#3CCCD3",
-        0.8: "#2A26CF",
-        1.0: "#8E1DCE"
-    }
+    useEffect(() => {
+        // max temp is 45, min temp is -30 -> +30 to clap values between 0 and 75
+        let maxGradVal = (1 - ( ( maxTemp + 30 ) / 75 )).toFixed(1)
+        let minGradVal = (1 - ( ( minTemp + 30 ) / 75 )).toFixed(1)
 
-    var usedGradientColors: any = [ ]
+        // console.log("max: " + maxGradVal + ", min: " + minGradVal)
 
-    for (let key in colorMappings) {
-        if (key == maxGradVal || (Number(key)+0.1).toFixed(1) == maxGradVal || (Number(key)-0.1).toFixed(1) == maxGradVal || 
-            key == minGradVal || (Number(key)+0.1).toFixed(1) == minGradVal || (Number(key)-0.1).toFixed(1) == minGradVal) 
-        {
-            usedGradientColors.push(colorMappings[key])
+        const colorMappings: any = {
+            0.0: "#E92020",
+            0.2: "#E7872D",
+            0.4: "#E1C627",
+            0.5: "#3ADE4E",
+            0.6: "#3CCCD3",
+            0.8: "#2A26CF",
+            1.0: "#8E1DCE"
         }
-    }
 
-    var gradientColors: any = []
-    var usedGradientColorsLen = usedGradientColors.length
+        var usedGradientColors: any = [ ]
 
-    for (let i = 0; i < usedGradientColorsLen; i++) {
-        if (i == 0) {
-            gradientColors.push({0:usedGradientColors[i]})
+        for (let key in colorMappings) {
+            if (key == maxGradVal || (Number(key)+0.1).toFixed(1) == maxGradVal || (Number(key)-0.1).toFixed(1) == maxGradVal || 
+                key == minGradVal || (Number(key)+0.1).toFixed(1) == minGradVal || (Number(key)-0.1).toFixed(1) == minGradVal) 
+            {
+                usedGradientColors.push(colorMappings[key])
+            }
         }
-        else if (i == usedGradientColorsLen-1) {
-            gradientColors.push({1:usedGradientColors[i]})
-        }
-        else {
-            let obj: any = {}
-            obj[i/(usedGradientColorsLen-1)] = usedGradientColors[i]
-            gradientColors.push(obj)
-        }
-    }
 
-    // console.log(gradientColors)
+        var usedGradientColorsLen = usedGradientColors.length
+
+        for (let i = 0; i < usedGradientColorsLen; i++) {
+            if (i == 0) {
+                gradientColors.push({0:usedGradientColors[i]})
+            }
+            else if (i == usedGradientColorsLen-1) {
+                gradientColors.push({1:usedGradientColors[i]})
+            }
+            else {
+                let obj: any = {}
+                obj[i/(usedGradientColorsLen-1)] = usedGradientColors[i]
+                gradientColors.push(obj)
+            }
+        }
+
+        // console.log(gradientColors)
+    }, [])
 
 
     return(
